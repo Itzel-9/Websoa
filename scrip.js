f8d9e601f0c8097c7a342ddf4651fd7c29e6a443
itzel <itzel.mendez0910@gmail.com>
2025-05-12 11:40:08 -0600

    Primera version

---
+++ scrip.js
@@ -0,0 +1,9 @@
+
+        // Cambiar el color de fondo del header cuando se le da clic al boton
+        function cambiarColorFondo() {
+            const header = document.querySelector("header");
+            header.style.backgroundColor = "#ff8cc6"; // rosa
+            header.style.transition = "background-color 0.5s";
+        }
+
+        
